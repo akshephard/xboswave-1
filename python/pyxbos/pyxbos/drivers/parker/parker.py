9a6d44250b1523e014e5262913055ea8c186b171
[akshephard] <akshephard@ucdavis.edu>
2019-03-25 21:59:22 -0700

    removing field not present in iot.proto

--- python/pyxbos/pyxbos/drivers/parker/parker.py
+++ python/pyxbos/pyxbos/drivers/parker/parker.py
@@ -127,7 +127,6 @@ class ParkerDriver(Driver):
                 time = int(time.time()*1e9),
                 parker_state = iot_pb2.ParkerState(
                     compressor_working_hours  =   types.Double(value=compressor_working_hours),
-                    buzzer_control  =   types.Int64(value=buzzer_control),
                     defrost_control  =   types.Int64(value=defrost_control),
                     start_resistors  =   types.Int64(value=start_resistors),
                     on_standby_status  =   types.Int64(value=on_standby_status),
