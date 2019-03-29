5b589164f50874d425eaacd9d00138f783054065
[akshephard] <akshephard@ucdavis.edu>
2019-03-29 16:37:34 -0700

    Updated the plugin with caps correct

--- ingester/plugins/parker_plugin.go
+++ ingester/plugins/parker_plugin.go
@@ -6,45 +6,46 @@ import (
 	xbospb "github.com/gtfierro/xboswave/proto"
 )
 
+
 func has_parker(msg xbospb.XBOS) bool {
 	return msg.XBOSIoTDeviceState.ParkerState != nil
 }
 
 var parker_units = map[string]string{
-	"compressor_working_hours":            "hours",
-	"on_standby_status":       "unknown",
-    "light_status":       "unknown",
-    "aux_output_status":       "unknown",
-	"next_defrost_counter": "seconds",
+	"CompressorWorkingHours":            "hours",
+	"OnStandbyStatus":       "unknown",
+    "LightStatus":       "unknown",
+    "AuxOutputStatus":       "unknown",
+	"NextDefrostCounter": "seconds",
 }
 
 var parker_lookup = map[string]func(msg xbospb.XBOS) (float64, bool){
 	// XBOSIoTDeviceState.ParkerState
-	"compressor_working_hours": func(msg xbospb.XBOS) (float64, bool) {
+	"CompressorWorkingHours": func(msg xbospb.XBOS) (float64, bool) {
 		if has_parker(msg) && msg.XBOSIoTDeviceState.ParkerState.compressor_working_hours != nil {
 			return float64(msg.XBOSIoTDeviceState.ParkerState.compressor_working_hours.Value), true
 		}
 		return 0, false
 	},
-	"on_standby_status": func(msg xbospb.XBOS) (float64, bool) {
+	"OnStandbyStatus": func(msg xbospb.XBOS) (float64, bool) {
 		if has_parker(msg) && msg.XBOSIoTDeviceState.ParkerState.on_standby_status != nil {
 			return float64(msg.XBOSIoTDeviceState.ParkerState.on_standby_status.Value), true
 		}
 		return 0, false
 	},
-	"light_status": func(msg xbospb.XBOS) (float64, bool) {
+	"LightStatus": func(msg xbospb.XBOS) (float64, bool) {
 		if has_parker(msg) && msg.XBOSIoTDeviceState.ParkerState.light_status != nil {
 			return float64(msg.XBOSIoTDeviceState.ParkerState.light_status.Value), true
 		}
 		return 0, false
 	},
-	"aux_output_status": func(msg xbospb.XBOS) (float64, bool) {
+	"AuxOutputStatus": func(msg xbospb.XBOS) (float64, bool) {
 		if has_parker(msg) && msg.XBOSIoTDeviceState.ParkerState.aux_output_status != nil {
 			return float64(msg.XBOSIoTDeviceState.ParkerState.aux_output_status.Value), true
 		}
 		return 0, false
 	},
-	"next_defrost_counter": func(msg xbospb.XBOS) (float64, bool) {
+	"NextDefrostCounter": func(msg xbospb.XBOS) (float64, bool) {
 		if has_parker(msg) && msg.XBOSIoTDeviceState.ParkerState.next_defrost_counter != nil {
 			return float64(msg.XBOSIoTDeviceState.ParkerState.next_defrost_counter.Value), true
 		}
