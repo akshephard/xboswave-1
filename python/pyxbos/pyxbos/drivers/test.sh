7099048be94cc15ea1e8f5954bcb73f50d4751ba
[akshephard] <akshephard@ucdavis.edu>
2019-04-03 19:18:53 -0700

    Trying to debug shellscript

--- python/pyxbos/pyxbos/drivers/test.sh
+++ python/pyxbos/pyxbos/drivers/test.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-
-echo "Positional Parameters"
-echo '$0 = ' $0
-echo '$1 = ' $1
-echo '$2 = ' $2
-echo '$3 = ' $3
 DRIVER_NAME=$1
 echo 'driver name is' $DRIVER_NAME
-ENTITY=$DRIVER_NAME".ent"
-echo $ENTITY
-
 wv mke -e 50y --nopassphrase -o $ENTITY
+echo "Made entity"
 wv inspect $ENTITY
+echo "Did inspection"
 wv rtgrant --subject $ENTITY -e 3y --attester $WAVE_DEFAULT_ENTITY --indirections 0 wavemq:publish,subscribe@solarplus/$DRIVER_NAME/*
+echo "Granted permissions"
 wv name --public --attester $ENTITY $NAMESPACE_HASH $NAMESPACE
+echo "linked namespace to hash"
 wv rtprove --subject $ENTITY -o driver_proof.pem wavemq:publish,subscribe@solarplus/$DRIVER_NAME/*
+echo "Did proof"
 wv verify driverproof.pem
+echo "Did verification"
