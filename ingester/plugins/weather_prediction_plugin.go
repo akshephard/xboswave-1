ed6bd51d8dbcf417b048ef5c2b6eaaa9a2bee291
[akshephard] <akshephard@ucdavis.edu>
2019-04-22 15:23:41 -0700

    Testing loop version of plugin

--- ingester/plugins/weather_prediction_plugin.go
+++ ingester/plugins/weather_prediction_plugin.go
@@ -74,7 +74,7 @@ func Extract(uri types.SubscriptionURI, msg xbospb.XBOS, add func(types.Extracte
             	if prediction.Visibility != nil {
             			extracted.Values = append(extracted.Values, float64(prediction.Visibility.Value))
                         //extracted_slice = append(extracted_slice, extracted.Values)
-                        extracted_slice[i] = extracted.Values
+                        extracted_slice[0] = extracted.Values
                         name_list = append(name_list, "visibility")
             		name = "visibility"
             	} else {
