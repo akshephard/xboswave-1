4fba00c7aee7020739bf10b16f4086134edee933
[akshephard] <akshephard@ucdavis.edu>
2019-04-22 17:49:32 -0700

    Adding temperature and ozone

--- ingester/plugins/weather_prediction_plugin.go
+++ ingester/plugins/weather_prediction_plugin.go
@@ -52,67 +52,44 @@ return types.ExtractedTimeseries{}
 
 func Extract(uri types.SubscriptionURI, msg xbospb.XBOS, add func(types.ExtractedTimeseries) error) error {
 	if msg.XBOSIoTDeviceState != nil {
+		if has_weather_station(msg) {
+			for name := range weather_lookup {
+				extracted := build_weather(uri, name, msg)
+				if err := add(extracted); err != nil {
+					return err
+				}
+			}
+		}
+
+		// proof of concept
+		// TODO: finish
 		if has_device(msg) {
-            //fmt.Printf("The length of the the message is: %d", len(msg.XBOSIoTDeviceState.WeatherPrediction.Predictions))
-            for _, _prediction := range msg.XBOSIoTDeviceState.WeatherStationPrediction.Predictions {
-            				prediction := _prediction.Prediction
-            				var extracted types.ExtractedTimeseries
-            				var name string
-            				time := int64(msg.XBOSIoTDeviceState.Time)
-            				step := (int64(_prediction.PredictionTime) - time) / 1e9
-            				extracted.Times = append(extracted.Times, time)
-            				if prediction.Temperature != nil {
-            					extracted.Values = append(extracted.Values, float64(prediction.Temperature.Value))
-            					name = "temperature"
-            				} else {
-            					continue
-            				}
-            				extracted.UUID = types.GenerateUUID(uri, []byte(name))
-            				extracted.Collection = fmt.Sprintf("xbos/%s", uri.Resource)
-            				extracted.Tags = map[string]string{
-            					"unit":            weather_units[name],
-            					"name":            name,
-            					"prediction_step": fmt.Sprintf("%d", step),
-            				}
-            				extracted.IntTags = map[string]int64{
-            					"prediction_time": int64(_prediction.PredictionTime),
-            				}
-            				if err := add(extracted); err != nil {
-            					return err
-            				}
-            			}
-            for _, _prediction := range msg.XBOSIoTDeviceState.WeatherStationPrediction.Predictions {
-            				prediction := _prediction.Prediction
-            				var extracted types.ExtractedTimeseries
-            				var name string
-            				time := int64(msg.XBOSIoTDeviceState.Time)
-            				step := (int64(_prediction.PredictionTime) - time) / 1e9
-            				extracted.Times = append(extracted.Times, time)
-            				if prediction.Temperature != nil {
-            					extracted.Values = append(extracted.Values, float64(prediction.Ozone.Value))
-            					name = "ozone"
-            				} else {
-            					continue
-            				}
-            				extracted.UUID = types.GenerateUUID(uri, []byte(name))
-            				extracted.Collection = fmt.Sprintf("xbos/%s", uri.Resource)
-            				extracted.Tags = map[string]string{
-            					"unit":            weather_units[name],
-            					"name":            name,
-            					"prediction_step": fmt.Sprintf("%d", step),
-            				}
-            				extracted.IntTags = map[string]int64{
-            					"prediction_time": int64(_prediction.PredictionTime),
-            				}
-            				if err := add(extracted); err != nil {
-            					return err
-            				}
-            			}
-
-
-
-
-
+			for _, _prediction := range msg.XBOSIoTDeviceState.WeatherStationPrediction.Predictions {
+				prediction := _prediction.Prediction
+				var extracted types.ExtractedTimeseries
+				var name string
+				time := int64(msg.XBOSIoTDeviceState.Time)
+				step := (int64(_prediction.PredictionTime) - time) / 1e9
+				extracted.Times = append(extracted.Times, time)
+				if prediction.Temperature != nil {
+					extracted.Values = append(extracted.Values, float64(prediction.Temperature.Value))
+					name = "temperature"
+				} else {
+					continue
+				}
+				extracted.UUID = types.GenerateUUID(uri, []byte(name))
+				extracted.Collection = fmt.Sprintf("xbos/%s", uri.Resource)
+				extracted.Tags = map[string]string{
+					"unit":            device_units[name],
+					"name":            name,
+					"prediction_step": fmt.Sprintf("%d", step),
+				}
+				extracted.IntTags = map[string]int64{
+					"prediction_time": int64(_prediction.PredictionTime),
+				}
+				if err := add(extracted); err != nil {
+					return err
+				}
 			}
 		}
 	}
