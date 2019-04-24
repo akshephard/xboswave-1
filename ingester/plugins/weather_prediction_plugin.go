5c76e5659670ae54a19c58a4a2d3d12dd445e97a
[akshephard] <akshephard@ucdavis.edu>
2019-04-23 20:38:02 -0700

    Adding newlines to the above

--- ingester/plugins/weather_prediction_plugin.go
+++ ingester/plugins/weather_prediction_plugin.go
@@ -61,9 +61,9 @@ func Extract(uri types.SubscriptionURI, msg xbospb.XBOS, add func(types.Extracte
 				var name string
 				time := int64(msg.XBOSIoTDeviceState.Time)
 				step := (int64(_prediction.PredictionTime) - time) / 1e9
-                fmt.Printf("The step is: %d", step)
-                fmt.Printf("The prediction time is: %d", int64(_prediction.PredictionTime))
-                fmt.Printf("The XBOSIoTDeviceState.Time is: %d", int64(msg.XBOSIoTDeviceState.Time))
+                fmt.Printf("The step is: %d\n", step)
+                fmt.Printf("The prediction time is: %d\n", int64(_prediction.PredictionTime))
+                fmt.Printf("The XBOSIoTDeviceState.Time is: %d\n", int64(msg.XBOSIoTDeviceState.Time))
 				extracted.Times = append(extracted.Times, time)
 				if prediction.Temperature != nil {
 					extracted.Values = append(extracted.Values, float64(prediction.Ozone.Value))
