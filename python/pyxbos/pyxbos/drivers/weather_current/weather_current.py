9e59c84ad1c1583f080d59df90713987f70a087d
[akshephard] <akshephard@ucdavis.edu>
2019-05-07 11:31:20 -0700

    updated message name to weatherstation

--- python/pyxbos/pyxbos/drivers/weather_current/weather_current.py
+++ python/pyxbos/pyxbos/drivers/weather_current/weather_current.py
@@ -32,7 +32,7 @@ class WeatherCurrentDriver(Driver):
         msg = xbos_pb2.XBOS(
             XBOSIoTDeviceState = iot_pb2.XBOSIoTDeviceState(
                 time = int(time.time()*1e9),
-                weather_current = weather_current_pb2.Weather_Current_State(
+                weather_current = weather_current_pb2.weather_station_pb2(
                     time  =   types.Int64(value=output.get('time',None)),
                     icon  =  output.get('icon',None),
                     nearestStormDistance  =   types.Double(value=output.get('nearestStormDistance',None)),
