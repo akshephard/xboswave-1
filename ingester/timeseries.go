b0f5d35823fc37f48f4f5adfff9baba0dbd2e8b1
[akshephard] <akshephard@ucdavis.edu>
2019-05-08 19:04:34 -0700

    Added prints into driver

--- ingester/timeseries.go
+++ ingester/timeseries.go
@@ -310,6 +310,7 @@ func (inf *influxClient) write(extracted types.ExtractedTimeseries) error {
         fmt.Printf("The fields contained are: %v\n", fields)
 
 		pt, err := influx.NewPoint(inf.collection, tags, fields, time.Unix(0, t))
+        print(pt)
 		if err != nil {
 			return errors.Wrap(err, "could not create new point")
 		}
